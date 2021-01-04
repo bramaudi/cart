e3b44a5bff200f3da4edaa4cc7707923ec295aee
Brama Udi <bramaudi.al@gmail.com>
2021-01-04 21:09:59 +0700

    New responsive design

--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -29,50 +29,58 @@ const Menu = () => {
       <h1>Halo Bos!</h1>
       <p style={{ padding: '0 5rem 0 0' }}>Yahhaha hayuk bro silahkan dipilih dulu ya cemilannya yahhaha:</p>
 
-      <h5 className="subtitle">🍴 Makanan</h5>
-      <div className="menus">
-        {foods.map(food => (
-          <div className="menu-item shadow">
-            <div className="title">
-              {food.name}
-              <div className="desc"><IconInfo /> {food.desc}</div>
+      <div class="divider"></div>
+
+      <div className="menu-wrapper">
+      <div className="menu-container">
+
+        <h5 className="subtitle">Makanan</h5>
+        <div className="menus">
+          {foods.map(food => (
+            <div className="menu-item shadow">
+              <div className="title">
+                {food.name}
+                <div className="desc"><IconInfo /> {food.desc}</div>
+              </div>
+              <div className="price">Rp.{food.price}</div>
+              <button
+                className="button add"
+                onClick={() => handleAddToCart(food)}
+                >
+                <svg fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+              </button>
             </div>
-            <div className="price">Rp.{food.price}</div>
-            <button
-              className="button add"
-              onClick={() => handleAddToCart(food)}
-              >
-              <svg fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
-            </button>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
 
-      <h5 className="subtitle">🍸 Minuman</h5>
-      <div className="menus">
-        {drinks.map(drink => (
-          <div className="menu-item shadow">
-            <div className="title">
-              {drink.name}
-              <div className="desc"><IconInfo /> {drink.desc}</div>
+        <h5 className="subtitle">Minuman</h5>
+        <div className="menus">
+          {drinks.map(drink => (
+            <div className="menu-item shadow">
+              <div className="title">
+                {drink.name}
+                <div className="desc"><IconInfo /> {drink.desc}</div>
+              </div>
+              <div className="price">Rp.{drink.price}</div>
+              <button
+                className="button add"
+                onClick={() => handleAddToCart(drink)}
+                >
+                <svg fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+              </button>
             </div>
-            <div className="price">Rp.{drink.price}</div>
-            <button
-              className="button add"
-              onClick={() => handleAddToCart(drink)}
-              >
-              <svg fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
-            </button>
-          </div>
-        ))}
+          ))}
+        </div>
+
+        <Cart
+          state={{ foods, drinks, cartPop, cartShow }}
+          set={{ setCartShow, setFoods, setDrinks }} />
+
+        <div className="cart-spacing"></div>
+        <img className="vector" src={vector} alt="Ilustrasi" />
+        
       </div>
-
-      <Cart
-        state={{ foods, drinks, cartPop, cartShow }}
-        set={{ setCartShow, setFoods, setDrinks }} />
-
-      <div className="cart-spacing"></div>
-			<img className="vector" src={vector} alt="Ilustrasi" />
+      </div>
 
     </section>
   )
